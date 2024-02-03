beda7a186c88631188926ee4415b4922aafa0b75
draysongz <dharmiegra4@gmail.com>
2024-02-03 23:06:03 +0100

    ff

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,7 +1,12 @@
 import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit';
 import { NextRequest, NextResponse } from 'next/server';
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 
 const NEXT_PUBLIC_URL = 'https://mfer-pi.vercel.app/';
+
+const sdk = new ThirdwebSDK("base-sepolia-testnet",{
+  secretKey:'w2JAM9-9TKVKS5e3OTl6GF2OjO_jzBsxVgP7UfnQxa1uUQCe6JOpm7Sdd3U2Dil87lVbXjYTRRv9hdzAYRB2Ng'
+});
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   let accountAddress: string | undefined = '';
@@ -14,17 +19,27 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     accountAddress = message.interactor.verified_accounts[0];
   }
 
-  if (body?.untrustedData?.inputText) {
-    text = body.untrustedData.inputText;
-  }
+ const contract = await sdk.getContract('0xD5F14F5cF40a1e3372A7BA47Eb384BF61A9aaA74')
 
-  console.log(body)
+ const token= await contract.erc20.totalSupply()
+ 
+
+ if(parseFloat(token.displayValue) > 0){
+  const mintToWallet = await contract.erc20.mintTo(accountAddress, 1000000)
+  if(mintToWallet){
+    
+  }
+ }
+ 
+
+
+ 
 
   return new NextResponse(
     getFrameHtmlResponse({
       buttons: [
         {
-          label: `Text: ${accountAddress}`,
+          label: `Text:  minted`,
         },
       ],
       image: `${NEXT_PUBLIC_URL}/park-2.png`,
