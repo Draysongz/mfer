72aaa72f2003caddc865853ca1d74c9e62d7b7a8
draysongz <dharmiegra4@gmail.com>
2024-02-03 18:35:05 +0100

    done

--- app/page.tsx
+++ app/page.tsx
@@ -6,22 +6,25 @@ const NEXT_PUBLIC_URL = 'https://mfer-git-main-brandai.vercel.app';
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: 'Click Me',
+      label: 'Claim Airdrop',
+      action: "post"
     },
+
+    {
+      label: 'support liquidity pool',
+      action: 'post_redirect'
+    }
   ],
   image: `${NEXT_PUBLIC_URL}/park-1.png`,
-  input: {
-    text: 'Tell me a boat story',
-  },
   post_url: `${NEXT_PUBLIC_URL}/api/frame`,
 });
 
 export const metadata: Metadata = {
-  title: 'zizzamia.xyz',
-  description: 'LFG',
+  title: 'Mfer.xyz',
+  description: 'A forcaster token',
   openGraph: {
-    title: 'zizzamia.xyz',
-    description: 'LFG',
+    title: 'Mfer.xyz',
+    description: 'A forcaster token',
     images: [`${NEXT_PUBLIC_URL}/park-1.png`],
   },
   other: {
